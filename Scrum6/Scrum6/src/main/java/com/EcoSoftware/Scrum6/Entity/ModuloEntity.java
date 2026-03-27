c6033d67929be928daf5dea0f99ada56bf68f9c1
Dana Castro <danacastro2014@gmail.com>
2026-03-27 03:08:03 -0500

    ajuste

--- Scrum6/Scrum6/src/main/java/com/EcoSoftware/Scrum6/Entity/ModuloEntity.java
+++ Scrum6/Scrum6/src/main/java/com/EcoSoftware/Scrum6/Entity/ModuloEntity.java
@@ -1,6 +1,10 @@
 package com.EcoSoftware.Scrum6.Entity;
 
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
 import com.fasterxml.jackson.databind.JsonNode;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,8 +12,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
